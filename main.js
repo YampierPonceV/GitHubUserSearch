71cdf3833a3bf7921239b0601bcb12f9f66770eb
yampiponce <yampiponce60@gmail.com>
2021-10-19 10:02:36 -0500

    update validation API - INPUT-SEARCH

--- main.js
+++ main.js
@@ -11,6 +11,10 @@ const txtButon = $("#text-search");
 getUser(user);
 
 btnBuscar.onclick = () => {
+  if (inputBuscar.value === "") {
+    inputBuscar.focus();
+    return alert("Ingrese un nombre de usuario");
+  }
   const user = inputBuscar.value;
   loaderIcon.classList.add("icon-loader-mostrar");
   txtButon.classList.add("text-search");
