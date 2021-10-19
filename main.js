09249eb98b8cf43d9d0e664e332f763a3889c0f2
yampiponce <yampiponce60@gmail.com>
2021-10-19 07:36:11 -0500

    Actualizando el onload dafault

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const $ = (selector) => document.querySelector(selector);
 
 const btnBuscar = $("#btn-sarch");
 const inputBuscar = $("#input-search");
+const user = "yampierponcev";
+
+getUser(user);
 
 btnBuscar.onclick = () => {
   const user = inputBuscar.value;
